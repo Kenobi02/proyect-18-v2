414e40c86e7c53d50fa32d4723ca2b300ba8df30
Kenobi02 <andreseddyrijo@outlook.com>
2025-05-17 22:41:36 -0400

    Update script.js

--- Proyect-Morita/script.js
+++ Proyect-Morita/script.js
@@ -1,13 +1,13 @@
 document.querySelector('.heart').addEventListener('click', function () {
     const xhr = new XMLHttpRequest();
-    xhr.open('GET', 'foto.pdf', true); // Asegúrate que el nombre del archivo sea correcto.
+    xhr.open('GET', 'carta para paulita.pdf', true); // Asegúrate que el nombre del archivo sea correcto.
     xhr.responseType = 'blob';
     xhr.onload = function () {
         if (xhr.status === 200) {
             const blob = xhr.response;
             const link = document.createElement('a');
             link.href = window.URL.createObjectURL(blob);
-            link.download = 'regalito.pdf'; // Nombre con el que se descarga la carta
+            link.download = 'feliz_cumple.pdf'; // Nombre con el que se descarga la carta
             link.click();
             window.URL.revokeObjectURL(link.href);
 
